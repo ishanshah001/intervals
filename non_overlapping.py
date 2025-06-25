bd1110ba77ea23ac41e1e59a3717d5a4d4ec1d21
is4761 <ishanpshah001@gmail.com>
2025-06-25 11:20:47 -0400

    nonoverlapping

---
+++ non_overlapping.py
@@ -0,0 +1,14 @@
+def eraseOverlapIntervals(intervals):
+
+    res = 0
+
+    intervals.sort(key=lambda x: x[1])
+    prev_end = intervals[0][1]
+
+    for i in range(1, len(intervals)):
+        if prev_end > intervals[i][0]:
+            res += 1
+        else:
+            prev_end = intervals[i][1]
+    
+    return res
